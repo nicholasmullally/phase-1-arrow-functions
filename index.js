61509909141e74f289bfead7a239b134a3bd2701
Nicholas <mullally.nt@gmail.com>
2022-03-27 14:42:13 -0700

    Done with assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = (parameter1, parameter2) => (parameter1 / parameter2);
+//let parameter1 = 2000;
+//let parameter2 = 100;
+//() => parameter1 / parameter2
+console.log(divide(2000, 100));
+
+const square = (x) => x * x;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
